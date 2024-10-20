ac700a8b26c5613766339cf05f110cb9efe76c5d
QuangHaii <haino@HINOBY>
2024-10-20 23:47:15 +0700

    Init architecture

---
+++ Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+	public interface IProductRepository
+	{
+		Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+		Task<Product> GetByIdAsync(string productId, CancellationToken cancellationToken = default);
+		void Insert(Product product);
+		void Remove(Product product);
+	}
+}
